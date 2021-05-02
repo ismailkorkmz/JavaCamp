c8d4ea4623ebdf39c3f04dbe00e810839b2fcaf4
ismailmurat korkmaz <348887@ogr.ktu.edu.tr>
2021-05-02 17:34:46 +0300

    day3

---
+++ inheritance/src/inheritance/CorporateCustomer.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+//kurumsal müþteriler için
+public class CorporateCustomer extends Customer{
+	String companyName; //þirket ismi gibi
+	String taxNumber; // vergi kimlik numarasý
+}
---
+++ inheritance/src/inheritance/IndividualCustomer.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+//Bireysel müþteriler için
+public class IndividualCustomer extends Customer {
+	String firstName;
+	String lastName;
+	String nationalIdentity; // tc kimlik
+
+}
